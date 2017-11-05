b2a53d05e33ab530a36912d81aa4f80d63f17d36
c2v4 <the_pro@wp.pl>
2017-11-05 20:33:57 +0100

    Initial commit

---
+++ desktop/src/main/java/com/c2v4/colonize/desktop/DesktopLauncher.java
@@ -0,0 +1,28 @@
+package com.c2v4.colonize.desktop;
+
+import com.badlogic.gdx.Files.FileType;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.c2v4.colonize.ColonizeGame;
+
+/** Launches the desktop (LWJGL) application. */
+public class DesktopLauncher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static LwjglApplication createApplication() {
+        return new LwjglApplication(new ColonizeGame(), getDefaultConfiguration());
+    }
+
+    private static LwjglApplicationConfiguration getDefaultConfiguration() {
+        LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+        configuration.title = "Colonize";
+        configuration.width = 640;
+        configuration.height = 480;
+        for (int size : new int[] { 128, 64, 32, 16 }) {
+            configuration.addIcon("libgdx" + size + ".png", FileType.Internal);
+        }
+        return configuration;
+    }
+}
---
+++ server/src/main/java/com/c2v4/colonize/server/ServerLauncher.java
@@ -0,0 +1,8 @@
+package com.c2v4.colonize.server;
+
+/** Launches the server application. */
+public class ServerLauncher {
+    public static void main(String[] args) {
+        // TODO Implement server application.
+    }
+}
