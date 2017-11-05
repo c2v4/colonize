0d324d296ca43a33d8bb32fe8d77d257b18a58eb
c2v4 <the_pro@wp.pl>
2017-11-05 22:49:37 +0100

    Added DI

---
+++ core/src/main/kotlin/com/c2v4/colonize/Context.kt
@@ -0,0 +1,15 @@
+package com.c2v4.colonize
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
+import com.badlogic.gdx.utils.viewport.FitViewport
+import com.github.salomonbrys.kodein.Kodein
+import com.github.salomonbrys.kodein.bind
+import com.github.salomonbrys.kodein.singleton
+
+val context = Kodein{
+    bind() from singleton { Stage( FitViewport(640F, 480F)) }
+    bind() from singleton { Skin(Gdx.files.internal("ui/skin.json")) }
+    bind() from singleton { ColonizeGame() }
+}
