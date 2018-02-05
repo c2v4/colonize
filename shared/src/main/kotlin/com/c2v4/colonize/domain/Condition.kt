8332fa5263df8a82800feff57ddc9fa54c13e067
c2v4 <the_pro@wp.pl>
2018-02-05 08:50:10 +0100

    Change Condition structure

--- shared/src/main/kotlin/com/c2v4/colonize/domain/Condition.kt
+++ shared/src/main/kotlin/com/c2v4/colonize/domain/Condition.kt
@@ -1,10 +1,9 @@
 package com.c2v4.colonize.domain
 
-sealed class Condition{
-    abstract fun invoke(): Boolean
+sealed class Condition {
+    abstract fun invoke(value: Int): Boolean
 }
 
-class AtMost:Condition(){
-    override fun invoke(): Boolean = true
-
+class AtMost(private val threshold:Int) : Condition() {
+    override fun invoke(value: Int): Boolean = (value <= threshold)
 }
