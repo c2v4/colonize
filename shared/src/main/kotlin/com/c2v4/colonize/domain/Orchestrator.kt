ce31a4756d8a9929d1a3b06856326560cd959a8e
c2v4 <the_pro@wp.pl>
2018-02-13 22:59:15 +0100

    Orchestrator with observer

--- shared/src/main/kotlin/com/c2v4/colonize/domain/Orchestrator.kt
+++ shared/src/main/kotlin/com/c2v4/colonize/domain/Orchestrator.kt
@@ -1,7 +1,13 @@
 package com.c2v4.colonize.domain
 
+import com.c2v4.colonize.util.checkArgument
 import java.util.*
 
 class Orchestrator(private var state: State = State(),private val actions:Deque<Action> = LinkedList()){
 
+    fun processAction(action: Action){
+        checkArgument(action.isApplicable(state))
+        state = action.invoke(state)
+        state.observers.filter { it.isAplicabe(action,state) }.forEach { processAction(it.react(action, state)) }
+    }
 }
