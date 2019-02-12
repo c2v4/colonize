8ff9d144bc2674b08ea9769568e90b4fb0317851
Mateusz <mateusz.duda.md@gmail.com>
2019-02-12 23:19:15 +0100

    Temperature increment event

---
+++ shared/src/main/kotlin/com/c2v4/colonize/domain/Action.kt
@@ -0,0 +1,26 @@
+package com.c2v4.colonize.domain
+
+sealed class Action {
+    abstract operator fun invoke(state: State): Pair<State, List<Action>>
+}
+
+const val TEMPERATURE_INCREMENT_VALUE = 2
+const val TERRAFORM_RATING_INCREMENT_FOR_GLOBAL_PARAMETERS = 1
+
+data class RaiseTemperature(val player: Player) : Action() {
+
+    override fun invoke(state: State): Pair<State, List<Action>> =
+        if (temperatureLens.get(state) == MAX_TEMPERATURE) state to emptyList()
+        else temperatureLens.modify(state) { it + TEMPERATURE_INCREMENT_VALUE } to listOf(
+            ChangeTerraformRating(
+                player,
+                TERRAFORM_RATING_INCREMENT_FOR_GLOBAL_PARAMETERS
+            )
+        )
+}
+
+data class ChangeTerraformRating(val player: Player, val amount: Int) : Action() {
+    override fun invoke(state: State): Pair<State, List<Action>> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
---
+++ shared/src/test/kotlin/com/c2v4/colonize/domain/RaiseTemperatureTest.kt
@@ -0,0 +1,31 @@
+package com.c2v4.colonize.domain
+
+import io.kotlintest.matchers.collections.shouldHaveSize
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldEqual
+import io.kotlintest.specs.AnnotationSpec
+
+internal class RaiseTemperatureTest:AnnotationSpec(){
+    
+    @Test
+    fun temperatureWasRaised(){
+        val player = Player()
+        val state = State()
+        val event = RaiseTemperature(player)
+        val (newState,events) = event(state)
+        (temperatureLens.get(newState) - temperatureLens.get(state)).shouldBe(TEMPERATURE_INCREMENT_VALUE)
+        events.shouldHaveSize(1)
+        events.first().shouldBe(ChangeTerraformRating(player, TERRAFORM_RATING_INCREMENT_FOR_GLOBAL_PARAMETERS))
+    }
+
+
+    @Test
+    fun temperatureWasNotRaisedWhenAlreadyMax(){
+        val player = Player()
+        val state = temperatureLens.set(State(), MAX_TEMPERATURE)
+        val event = RaiseTemperature(player)
+        val (newState,events) = event(state)
+        temperatureLens.get(newState).shouldBe(temperatureLens.get(state))
+        events.shouldHaveSize(0)
+    }
+}
