0415568b92327fb4c46b476647147e777dc8b73c
Mateusz <mateusz.duda.md@gmail.com>
2019-02-18 00:58:33 +0100

    Code issues

--- shared/src/main/kotlin/com/c2v4/colonize/domain/map/HexCoordinate.kt
+++ shared/src/main/kotlin/com/c2v4/colonize/domain/map/HexCoordinate.kt
@@ -1,6 +1,6 @@
 package com.c2v4.colonize.domain.map
 
-class HexCoordinate private constructor(val x: Int, val z: Int) {
+data class HexCoordinate constructor(val x: Int, val z: Int) {
     val y
         get() = z - x
 
@@ -10,6 +10,7 @@ class HexCoordinate private constructor(val x: Int, val z: Int) {
 
         fun of(q: Int, r: Int): HexCoordinate =
             HexCoordinate(q, r)
+
         fun of(x: Int, y: Int, z: Int): HexCoordinate =
             if (x + y + z == 0) HexCoordinate(x, y) else throw IllegalArgumentException()
     }
@@ -17,26 +18,4 @@ class HexCoordinate private constructor(val x: Int, val z: Int) {
     fun neighbours(): Set<HexCoordinate> = NEIGHBOUR_COORDINATES.map { it + this }.toSet()
 
     operator fun plus(other: HexCoordinate) = of(x + other.x, z + other.z)
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as HexCoordinate
-
-        if (x != other.x) return false
-        if (y != other.y) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = x
-        result = 31 * result + y
-        return result
-    }
-
-    override fun toString(): String {
-        return "HexCoordinate(x=$x, y=$y,z = $z)"
-    }
 }
