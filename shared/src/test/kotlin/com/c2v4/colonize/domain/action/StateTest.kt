63ab8fdbed4ddc6f8350fc99acf0319ecf03761e
Mateusz <mateusz.duda.md@gmail.com>
2019-02-21 21:55:52 +0100

    Replaced Mockito with Mockk

--- shared/src/test/kotlin/com/c2v4/colonize/domain/action/StateTest.kt
+++ shared/src/test/kotlin/com/c2v4/colonize/domain/action/StateTest.kt
@@ -3,18 +3,18 @@ package com.c2v4.colonize.domain.action
 import com.c2v4.colonize.domain.*
 import com.c2v4.colonize.domain.map.MAX_NUMBER_OF_OCEANS
 import com.c2v4.colonize.domain.map.SurfaceMap
-import com.nhaarman.mockitokotlin2.mock
-import com.nhaarman.mockitokotlin2.whenever
 import io.kotlintest.matchers.boolean.shouldBeFalse
 import io.kotlintest.matchers.boolean.shouldBeTrue
 import io.kotlintest.specs.AnnotationSpec
+import io.mockk.every
+import io.mockk.mockk
 
 internal class StateTest : AnnotationSpec() {
 
     @Test
     internal fun endGamePositive() {
-        val surfaceMapMock = mock<SurfaceMap>()
-        whenever(surfaceMapMock.numberOfOceans).thenReturn(MAX_NUMBER_OF_OCEANS)
+        val surfaceMapMock = mockk<SurfaceMap>()
+        every{surfaceMapMock.numberOfOceans } returns MAX_NUMBER_OF_OCEANS
         endGameCheck(
             State(
                 globalParameters = GlobalParameters(
